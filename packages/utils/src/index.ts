95d89e7f138eda6639b742f23f66162657cd26df
omaroskars <omar@konstant.is>
2025-05-21 16:37:06 +0200

    get nester prop

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -3,4 +3,5 @@ export * from "./createObjectKeys.js";
 export * from "./format.js";
 export * from "./mapAsync.js";
 export * from "./getClientSideUrl.js";
+export * from "./getNestedProperty.js";
 export * from "./getServerSideUrl.js";
