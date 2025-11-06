7fa7046da747cc04340f506887bdfb4eeeb130bf
omaroskars <omar@konstant.is>
2025-11-06 21:19:24 +0100

    delete apps

--- apps/web/src/app/(payload)/admin/importMap.js
+++
@@ -1,5 +0,0 @@
-import { SlugFieldClient as SlugFieldClient_992eb75ac779e495436f30bdf1725992 } from '@konstant/payload/client'
-
-export const importMap = {
-  "@konstant/payload/client#SlugFieldClient": SlugFieldClient_992eb75ac779e495436f30bdf1725992
-}
