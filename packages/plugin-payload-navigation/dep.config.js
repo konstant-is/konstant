32928b8998d51d6e05c797cc08d10a025487c3bb
omaroskars <omar@konstant.is>
2026-01-08 19:49:52 +0100

    add payloadmcs ui to deps

--- packages/plugin-payload-navigation/dep.config.js
+++ packages/plugin-payload-navigation/dep.config.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   // These must be provided by the consuming project
-  peer: ['payload', '@payloadcms/plugin-nested-docs'],
+  peer: ['payload', '@payloadcms/plugin-nested-docs', '@payloadcms/ui'],
 
   // These are bundled with your library and needed at runtime
   runtime: ['slugify'],
