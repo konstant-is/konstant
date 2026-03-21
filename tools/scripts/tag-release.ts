8a3e5f985fca01b888a3611cfdf5ecd89f9a2354
omaroskars <omar@konstant.is>
2026-03-21 20:19:53 +0100

    update to next 16

--- tools/scripts/tag-release.ts
+++ tools/scripts/tag-release.ts
@@ -1,21 +1,22 @@
-import { createTag, getVersion } from "./utils";
-import { execSync } from "child_process";
+import { execSync } from 'child_process'
+
+import { createTag, getVersion } from './utils'
 
 const run = () => {
   try {
-    const tag = createTag(getVersion());
+    const tag = createTag(getVersion())
     // Execute Git commands to create and push the tag
-    console.log(`Creating Git tag: ${tag}`);
+    console.log(`Creating Git tag: ${tag}`)
 
-    execSync(`git tag -a ${tag} -m "Release ${tag}"`);
-    execSync("git push --follow-tags");
-    console.log(`Changes committed and tagged as ${tag}`);
+    execSync(`git tag -a ${tag} -m "Release ${tag}"`)
+    execSync('git push --follow-tags')
+    console.log(`Changes committed and tagged as ${tag}`)
 
-    console.log(`${tag} pushed to remote successfully`);
-  } catch (error) {
-    console.error("Error creating Git tag:", error.message);
-    process.exit(1);
+    console.log(`${tag} pushed to remote successfully`)
+  } catch (error: any) {
+    console.error('Error creating Git tag:', error.message)
+    process.exit(1)
   }
-};
+}
 
-run();
+run()
